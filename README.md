1c96e28c67c6ecb95950983c1201ca1d2afb9dce
vincenttommi <vincenttommi@gmail.com>
2023-09-20 12:57:11 +0300

    writing methods in python

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # to--do--django--app
+# Django is a high-level Python Web framework-based web framework that allows rapid development and clean, pragmatic design. today we will create a todo app created to understand the basics of Django. In this web app, one can create notes like Google Keep or Evernote
--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 
-# Create your views here.
+
+
+from .forms import TodoForm
+from .models import Todo
+
+#begninging of writing functions
+
+#function to get todo items
+def index(request):
+    item_list  = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+        
+    form  = TodoForm()
+    
+    page  = {
+        "forms":form,
+        "list":item_list,
+        "title":"TODO LIST",
+    }    
+    return render(request, 'todo/index.html',page)
+
+
+#funtion to remove item, it receive todo items_id as primary key from url 
+def remove(request, item_id):
+    item  = Todo.objects.get(id=item_id)
+    item.delete()
+    
+    messages.info(request,"item removed !!!")
+    
+    return redirect('todo')
+
+    
